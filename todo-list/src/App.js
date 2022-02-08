075e15af377957b71341991d3c87b099786d444e
Horacio Torres Galindo <thoraciog@gmail.com>
2022-02-07 22:28:31 -0700

    TodoList Submission

---
+++ todo-list/src/App.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import Header from './Header.js';
+import './App.css';
+
+
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState(" ");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+
+function Todo({ todo, index, completeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button className="completeButton" onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+
+  const [todos, setTodos] = useState([
+    {
+      text: "Check in beach-side hotel",
+      isCompleted: false
+    },
+    {
+      text: "Get some drinks",
+      isCompleted: true
+    },
+    {
+      text: "Go Scuba Diving",
+      isCompleted: false
+    }
+  ]);
+
+  return (
+
+    <div className="bg-image">
+      <Header />
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+            />
+          ))}
+          <TodoForm addTodo={addTodo} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
