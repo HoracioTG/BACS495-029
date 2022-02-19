9cc7dc3ceaba05fafcd539aa59aaaf46e94f11b5
Horacio Torres Galindo <thoraciog@gmail.com>
2022-02-18 20:19:15 -0700

    first working api with all criteria

--- myApi/routes/students.js
+++ myApi/routes/students.js
@@ -1,28 +1,13 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
 
+const router = Router();
 
-var students = [
-  { id: 1, name: "Horacio Torres" },
-  { id: 2, name: "Anderson Joe" },
-  { id: 3, name: "Peter Griffin" }
-];
-
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send("idk");
+router.get('/', (req, res) => {
+  return res.send(Object.values(req.context.models.users));
 });
 
-// app.post('/', (req, res) => {
-//   res.send('Got a POST request')
-// })
+router.get('/:userId', (req, res) => {
+  return res.send(req.context.models.users[req.params.userId]);
+});
 
-// app.put('/user', (req, res) => {
-//   res.send('Got a PUT request at /user')
-// })
-
-// app.delete('/user', (req, res) => {
-//   res.send('Got a DELETE request at /user')
-// })
-
-module.exports = router;
+export default router;
